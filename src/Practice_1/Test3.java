e69c1302e5eb81a725b9ad59fe997a0654a7fe9c
SabryneRjeb <sabryne.rjeb@gmail.com>
2021-01-23 22:24:12 -0500

    second commit

---
+++ src/Practice_1/Test3.java
@@ -0,0 +1,10 @@
+package Practice_1;
+
+public class Test3 {
+    public static void main(String[] args) {
+        // do some work
+        // more work
+        // whatever extra work
+        // im done ready to share
+    }
+}
